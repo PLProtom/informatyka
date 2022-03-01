19df07e7d8d759fbf431d2e3f5b30ab9eb9a2dba
PLProtom <agent0tomek@gmail.com>
2022-03-01 11:02:06 +0100

    dodano zadanie "Algorytm wyznaczania liczb pierwszych"

---
+++ Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int lp = 0;
+    int p = 2;
+    int d = 2;
+
+    cout << "Ile liczb pierwszych wyswietlic?\n";
+    cin >> n;
+    cout << "Liczby pierwsze: ";
+
+    while (lp < n)
+    {
+        while (d < p)
+        {
+            if (p % d == 0)
+            {
+                cout << p << ", ";
+                lp++;
+            }
+        }
+        p++;
+    }
+}
