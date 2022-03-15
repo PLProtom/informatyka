4a9a9ab9697fd357be8debfab2d999657ef31e44
PLProtom <agent0tomek@gmail.com>
2022-03-15 11:10:04 +0100

    NWW i NWD

---
+++ NWD-NWW/NWD-NWW.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int a = 0;
+int b = 0;
+int iloczyn = 0;
+int x = 0;
+int y = 0;
+int nww = 0;
+
+// Najwiekszy wspolny dzielnik
+int NWD()
+{
+    cout << "Podaj pierwsza licze: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    x = a;
+    y = b;
+
+    while (a != b) 
+    {
+        if (a < b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+    }
+    return a;
+}
+
+// Najmniejsza wspolna wielokrotnosc
+int NWW()
+{
+    iloczyn = x * y;
+    nww = iloczyn / a;
+    return nww;
+}
+
+int main()
+{
+    NWD();
+    cout << "Najwiekszy wspolny dzielnik: " << a << endl;
+    NWW();
+    cout << "Najmniejsza wspolna wielokrotnosc: " << nww << endl;
+}
