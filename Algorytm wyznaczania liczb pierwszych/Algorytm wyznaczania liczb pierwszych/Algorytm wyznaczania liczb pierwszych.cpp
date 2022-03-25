3ebfa10940d1334d916a8948333d18f55c2c455e
Protom <agent0tomek@gmail.com>
2022-03-25 18:07:50 +0100

    Algorytm wyznaczania liczb pierwszych

---
+++ Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych/Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, lp, d, p;
+    bool pierwsza;
+    p = 2;
+    lp = 0;
+
+    cout << "Ile liczb pierwszych wyswietlic?\n";
+    cin >> n;
+    cout << "Liczby pierwsze: ";
+
+    while (lp < n)
+    {
+        pierwsza = true;
+        for (d = 2; d < p; d++)
+        {
+            if (p % d == 0)
+            {
+                pierwsza = false;
+                break;
+            }
+        }
+
+        if (pierwsza)
+        {
+            cout << p << " ";
+            lp++;
+        }
+        p++;
+    }
+}
