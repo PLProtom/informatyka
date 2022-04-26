e6ec670589fef6fdc4debe453470ae8ccc254ccd
Protom <49395761+PLProtom@users.noreply.github.com>
2022-04-26 10:58:02 +0200

    Dodano test wydajności algorytmu

--- Ciag Fibonacciego/Ciag Fibonacciego.cpp
+++ Ciag Fibonacciego/Ciag Fibonacciego.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
+#include <chrono>
 using namespace std;
+
+chrono::duration<double> elapsed_seconds;
 
 int fibonacci(int n)
 {
@@ -12,12 +15,25 @@ int fibonacci(int n)
 
 int main()
 {
+
     int a;
     cout << "Ile wyrazow ciagu wypisac: ";
     cin >> a;
 
+    chrono::system_clock::time_point start = chrono::system_clock::now();	// czas start
+
     for(int i = 0; i < a; i++)
     {
-        cout << fibonacci(i) << endl;
+        cout << i + 1 << ". " << fibonacci(i) << endl;
     }
+
+    chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+
+    elapsed_seconds += end - start;	// obliczenie czasu
+    cout << "\nCzas: " << endl;
+    // wyswietlenie czasu w roznych jednostkach
+    cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count() << "\t milliseconds\n";
+    cout << std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count() << "\t seconds\n";
+    //cout <<  elapsed_seconds.count() << " second" << endl;
+    cout << std::chrono::duration_cast<std::chrono::minutes>(elapsed_seconds).count() << "\t minutes\n";
 }
