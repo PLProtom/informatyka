05793eaef5cfbe6d817fbbeade7a388930a60a71
PLProtom <agent0tomek@gmail.com>
2022-05-24 10:58:05 +0200

    Wyszukiwanie binarne

--- Wyszukiwanie binarne/Wyszukiwanie binarne.cpp
+++ Wyszukiwanie binarne/Wyszukiwanie binarne.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
+#include <chrono>
 using namespace std;
+
+chrono::duration<double> elapsed_seconds;
 
 int szukaj(int szukana, int tab[15])
 {
@@ -22,6 +25,8 @@ int szukaj(int szukana, int tab[15])
         {
             l = sr + 1;
         }
+
+        sr = (l + p) / 2;
     }
 }
 
@@ -32,5 +37,14 @@ int main()
 
     cout << "Podaj szukana liczbe:\n";
     cin >> szukana;
+    chrono::system_clock::time_point start = chrono::system_clock::now();	// czas start
     cout << "Liczba " << szukana << " wystepuje w zbiorze w komorce o indeksie " << szukaj(szukana, tab) << endl;
+
+
+    chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+    elapsed_seconds += end - start;	// obliczenie czasu
+    cout << "\nCzas: " << endl;
+    cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count() << "\t milliseconds\n";
+    cout << std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count() << "\t seconds\n";
+    cout << std::chrono::duration_cast<std::chrono::minutes>(elapsed_seconds).count() << "\t minutes\n";
 }
